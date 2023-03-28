8523a55fc756a49bc3ed32019934b942dfccd086
Umang A <umanga.eng@gmail.com>
2023-03-27 20:55:51 -0400

    feat: login page created

--- pages/login.js
+++ pages/login.js
@@ -1,7 +1,26 @@
-function Login() {
+import styles from "../pages/login.module.css"
+import {getProviders} from "next-auth/react"
+
+export default function Login({providers}) {
+    console.log(providers)
   return (
-    <div>Login</div>
+    <div className={styles.loginPage}>
+        <div className={styles.loginMenu}>
+            <div className={styles.loginTitle}>
+                <h1 className="text-xl">Platform Login</h1>
+                <h2>Use any authentication providers to start using the platform</h2>
+            </div>
+            {Object.values(providers).map(provider => (<div key={provider.id}>
+                <button className={styles.loginButton}>Sign in with {provider.name}</button>
+                </div>))}
+        </div>
+    </div>
   )
 }
 
-export default Login
+export async function getServerSideProps(){
+    const providers = await getProviders();
+    return{
+        props: {providers}
+    }
+}
