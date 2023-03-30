ecdfd0950a680b6bf2b8631bafa2531a1788c100
Umang A <umanga.eng@gmail.com>
2023-03-30 10:38:56 -0400

    feat: next auth configured

--- pages/api/auth/[...nextauth].js
+++ pages/api/auth/[...nextauth].js
@@ -1,12 +1,30 @@
 import NextAuth from "next-auth"
 import GoogleProvider from "next-auth/providers/google";
+import { MongoDBAdapter } from "@next-auth/mongodb-adapter"
+import clientPromise from "../../../lib/mongodb"
 
 export const authOptions = {
+    adapter: MongoDBAdapter(clientPromise),
     providers: [
         GoogleProvider({
             clientId: process.env.GOOGLE_CLIENT_ID,
             clientSecret: process.env.GOOGLE_CLIENT_SECRET
         })
-        ]
+        ],
+    pages:{
+        signIn: "/login",
+    },
+    callbacks:{
+        // cannot log token or session
+        session: async ({token, session}) => {
+            if(session?.user && token?.sub){
+                session.user.id = token.sub
+                // console.log("current token: " + token)
+            }
+            console.log("no token found: " + token)
+            return session
+        }
+    }
 }
+
 export default NextAuth(authOptions)
