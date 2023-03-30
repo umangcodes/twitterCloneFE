b3de05ec98c42e968a8ae9883a29e0b8edd1cc64
Umang A <umanga.eng@gmail.com>
2023-03-30 11:24:35 -0400

    chore: log disabled

--- pages/api/auth/[...nextauth].js
+++ pages/api/auth/[...nextauth].js
@@ -9,7 +9,7 @@ export const authOptions = {
         GoogleProvider({
             clientId: process.env.GOOGLE_CLIENT_ID,
             clientSecret: process.env.GOOGLE_CLIENT_SECRET
-        })
+        }),
         ],
     pages:{
         signIn: "/login",
@@ -21,7 +21,7 @@ export const authOptions = {
                 session.user.id = token.sub
                 // console.log("current token: " + token)
             }
-            console.log("no token found: " + token)
+            // console.log("no token found: " + token)
             return session
         }
     }
